76c77b8ed12f7c29b658eafb5b5cf9e5eec8f2ba
Harsh gautam <androidmasterid>
2022-06-26 16:27:44 +0530

    basic for node

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const express = require('express');
 
 const app = express();
 const studentRoutes = require('./api/routes/student') 
+const facultyRoutes = require('./api/routes/faculty') 
 
 
 app.use('/student' , studentRoutes)
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'My app is running again  ',
-  });
-});
+app.use('/faculty' , facultyRoutes)
+// app.use((req, res, next) => {
+//   res.status(200).json({
+//     message: 'My app is running again  ',
+//   });
+// });
+
+app.use((req , res , next)=>(
+  res.status(404).json({
+    error: 'Bad Request'
+  })
+))
 
 module.exports = app;
